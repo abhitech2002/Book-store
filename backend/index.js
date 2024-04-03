f28749cee8c386b820a6abe8a6c9c58ce529a346
abhitech2002 <oabhishek31@gmail.com>
2024-04-03 15:07:40 +0530

    Adding Cors

--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,24 @@ import { PORT, mongoDBURL } from "./config.js";
 import mongoose from 'mongoose'
 import { Book } from "./models/bookModel.js";
 import bookRoute from "./route/bookRoutes.js";
+import cors from 'cors'
 
 const app = express()
 
 // Middleware for parsing router body
 app.use(express.json())
 
+// Routes
 app.use('/books', bookRoute)
+
+// Middleware for handling CORS policy
+app.use(    
+    cors({
+        origin: 'http://localhost:3000', 
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type', 'Authorization']
+    })
+)
 
 // Mongoose for connecting to mongoDB Database
 mongoose
